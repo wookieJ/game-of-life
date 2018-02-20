9e3f10db2b0d926991e167cc9bc569fac2106e83
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-02-21 00:14:51 +0100

    Color cells

--- scripts/game.js
+++ scripts/game.js
@@ -1,49 +1,84 @@
 const canvasElem = document.getElementById('canvas');
 const ctx = canvasElem.getContext('2d');
 
-const planeWidth = 800;
-const planeHeight = 500;
+const planeWidth = 900;
+const planeHeight = 900;
 
-// dodać zabezpieczenie przed rozjeżdżaniem się dla niepodzielnych bez reszty wymiarów
-
-function Grid(w, h) {
+/**
+ * Grid - plansza gry w życie
+ */
+function Grid(w, h)
+{
     this.width = w;
     this.height = h;
     this.step = w < h ? w/(w<250 ? 10 : 50) : h/(h<250 ? 10 : 50);
+    this.grid = [];
+    
+    for(i=0 ; i<this.width ; i++)
+    {
+        this.grid[i] = [];
+        for(j=0 ; j<this.height ; j++)
+            this.grid[i][j] = 0;
+    }
+    
+    console.log(">> grid (" + this.width + ";" + this.height + ") created");
 }
 
-Grid.prototype.draw = function()
+/**
+ * Funkcja sprawdzająca stan pól planszy
+ */
+Grid.prototype.checkCellValue = function()
 {
-    for(i=0 ; i<=this.width ; i+=this.step)
+    for(i=0 ; i<this.width ; i++)
     {
-        ctx.beginPath();
-        ctx.moveTo(i,0);
-        ctx.lineTo(i,this.height);
-        ctx.stroke();
-    }
-    for(i=0 ; i<=this.height ; i+=this.step)
-    {
-        ctx.beginPath();
-        ctx.moveTo(0,i);
-        ctx.lineTo(this.width,i);
-        ctx.stroke();
+        for(j=0 ; j<this.height ; j++)
+        {
+            if(this.grid[i][j] == 1)
+            {
+                ctx.beginPath();
+                ctx.fillStyle = "black";
+                ctx.rect(i*this.step, j*this.step, this.step, this.step);
+                ctx.fill();
+                ctx.stroke();
+            }
+            else
+            {
+                ctx.beginPath();
+                ctx.fillStyle = "white";
+                ctx.rect(i*this.step, j*this.step, this.step, this.step);
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
     }
 }
 
+/*
+ *  Setup function
+ */
 function setup()
 {
-    // canvas sizes
+    // rozmiar planszy
     canvasElem.width = planeWidth;
     canvasElem.height = planeHeight;
     canvasElem.style.backgroundColor = "white"
     
+    // stworzenie siatki
     grid = new Grid(planeWidth, planeHeight);
-    grid.draw();
+    //grid.draw();
+    console.log(grid.grid[0][0]);
+    grid.grid[15][15] = 1;
+    grid.checkCellValue();
+//    grid.addRandomCells();
 }
 
-//function run()
-//{
-//}
+/*
+ *  Game loop
+ */
+function run()
+{
+    
+}
 
 setup();
-//setInterval(run,100);
+setInterval(run,100);
